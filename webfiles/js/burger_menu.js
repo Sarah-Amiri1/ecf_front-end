9e94a83400d5f396ce76d59f00bc17870f671f1c
Sarah-Amiri1 <amiri.sarah@outlook.com>
2025-02-20 14:56:14 +0100

    Retouche du burger menu

--- webfiles/js/burger_menu.js
+++ webfiles/js/burger_menu.js
@@ -13,22 +13,20 @@
 //     }
 // });
 
-
 const btnBurger = document.querySelector("#burger-icon");
+const btnClose = document.querySelector("#burger-close");
 const nav2 = document.querySelector(".nav2");
 
-if (btnBurger && nav2) {
-    btnBurger.addEventListener("click", () => {
-        nav2.classList.toggle("active");
-        btnBurger.classList.toggle("open");
-    });
+// Ouvrir le menu
+btnBurger.addEventListener("click", () => {
+    nav2.classList.add("active");
+    btnBurger.style.display = "none";
+    btnClose.style.display = "block";
+});
 
-    document.addEventListener("click", (e) => {
-        if (!btnBurger.contains(e.target) && !nav2.contains(e.target)) {
-            nav2.classList.remove("active");
-            btnBurger.classList.remove("open");
-        }
-    });
-} else {
-    console.error("Erreur : Élément .nav2 non trouvé !");
-}
+// Fermer le menu
+btnClose.addEventListener("click", () => {
+    nav2.classList.remove("active");
+    btnBurger.style.display = "block";
+    btnClose.style.display = "none";
+});
